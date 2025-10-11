08213432003b273c6c4a189e821af5de4c89b7ab
Jordan BARDET--TECHER <jordan.bardettecher@etu.unilim.fr>
2025-10-11 18:04:07 +0200

    exo 2 à 4

--- H.py
+++ H.py
@@ -1,4 +1,9 @@
 
+from collision_preimages import trouver_collision, trouver_preimage
+from hachage_xor import H_star
+from attaques_cibles import trouver_collisions_dictionnaire
+
+
 if __name__ == "__main__":
 
     car: str = input("Veuillez enterer un unique caractère : ")
@@ -8,4 +13,12 @@ if __name__ == "__main__":
     
     print(f"Le caractère {car.encode('utf-8').hex()} est valide.")
 
-    
+    print("=== Recherche de collision ===")
+    trouver_collision()
+
+    print("\n=== Recherche de préimage ===")
+    trouver_preimage()
+
+    texte = input("\nEntrez un texte : ")
+    print(f"H*(\"{texte}\") = {H_star(texte)}")
+    trouver_collisions_dictionnaire("dictionnaire.txt")
